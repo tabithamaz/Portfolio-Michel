66921c80af36840da4c59ddab03b46a670ee33f5
tabithamaz <joycemanzanza@gmail.com>
2023-02-09 11:43:16 +0100

    init

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+ 
+
+ 
+function Header(){
+    return(
+    <div className="contener">
+    
+    <img src ="" alt=""/>
+    <nav className="navigation">
+    <ul>
+    <li> <span> 01.</span><a href="about"> About</a></li>
+    <li><span> 02.</span><a href="experience">Experiences </a></li>
+    <li><span> 03.</span><a href="work">Work </a></li>
+    <li><span> 04.</span><a href="work">Contact </a></li>
+    </ul>
+    </nav>
+   
+    <button> Resume</button>
+        </div>
+    );
+}
+export default Header;
